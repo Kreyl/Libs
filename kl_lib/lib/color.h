2f3c1bdeca00d66c7d0bd1aa3d3eda42b9e2f763
kreyl <laystoll@yandex.ru>
2020-03-05 10:28:26 +0300

    + LedWBrt

--- kl_lib/lib/color.h
+++ kl_lib/lib/color.h
@@ -30,8 +30,20 @@ static inline uint32_t ClrCalcDelay(uint16_t AValue, uint32_t Smooth) {
 }
 
 // Calculate Smooth value from desired duration of switching
-static inline int32_t CalcSmooth_st_from_ms(int32_t Duration_ms) {
+// ?
+static int32_t CalcSmooth_st_from_ms(int32_t Duration_ms) {
     return (TIME_MS2I(Duration_ms) * 10L) / 36L;
+}
+
+// 34000 ms max
+static int32_t CalcSmoothFrom_ms(int32_t Duration_ms) {
+    return (Duration_ms * 58788L) / 4096L;
+}
+
+static int32_t CalcSmoothFrom_ms_UsingBrt(int32_t Duration_ms, int32_t Brt1, int32_t Brt2) {
+
+    return (Duration_ms * 58788L) / 4096L;
+
 }
 
 struct Color_t {
