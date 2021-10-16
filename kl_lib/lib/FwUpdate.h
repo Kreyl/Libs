b57e94e4be470b455b2818bcde0bde0782cae34b
Eldalim\Elessar <elessarcuthalion@mail.ru>
2021-10-16 22:52:19 +0200

    обновлён FwUpdate: добавлена возможность чтения с диска для последующей отправки по UART; добавлены примеры использования

--- kl_lib/lib/FwUpdate.h
+++ kl_lib/lib/FwUpdate.h
@@ -5,7 +5,6 @@
 //#define UpdateFromDisk
 
 // Constants, see datasheet
-#define FLASH_START_ADDR        0x08000000UL
 #define FLASH_PAGE_SZ           2048L       // bytes in page, see datasheet
 #define PAGE_SZ_WORD64          (FLASH_PAGE_SIZE/8)
 
@@ -38,6 +37,9 @@ public:
 #ifdef UpdateFromDisk
     uint8_t CheckFileOnDisk(const char *AFileName);
     uint8_t ReadFileOnDisk();
+    uint8_t ReadFileOnDiskToBuff(uint32_t &BytesCnt, uint16_t &CrcOut);
+    uint8_t CalcCrcOfFullyFile(uint16_t &CrcOut);
+    uint8_t DeliteFileOnDisk();
 #endif
     void SwitchAndRun();
 };
@@ -49,3 +51,126 @@ extern FwUpdater_t FwUpdater;
 
 uint32_t CheckFWfile (const char *AFileName);
 uint8_t ReadFWfile (uint32_t TotalLen);
+
+
+// =============================== Examples ====================================
+/*
+ *  === Upgrade from disk ===
+#define MainFwFileName_PATTERN  "FwMain*.bin"
+
+    // Preparations
+    if (FwUpdater.CheckFileOnDisk(MainFwfileName_PATTERN) == retvOk) {
+        FwUpdater.Restart();
+        Printf("Start FW Update\r\n");
+        // Reading and flashing
+        if (FwUpdater.ReadFileOnDisk() == retvOk) {
+            Printf("FW Update completed\r\n");
+            chThdSleepMilliseconds(45);
+            FwUpdater.SwitchAndRun();
+        }
+    }
+ *
+ *
+ *  === Upgrade from UART (TX) ===
+#define TopFwFileName_PATTERN   "FwTop*.bin"
+
+    // Preparations
+    if (FwUpdater.CheckFileOnDisk(TopFwFileName_PATTERN) == retvOk) {
+        Printf("Start Top FW Update\r\n");
+        TopFwUpdate();
+    }
+
+void TopFwUpdate() {
+    Iwdg::Reload();
+    // Initiate update
+    if (RS422.SendCmd(TIMEOUT_SHORT_ms, 1, "UpdateFwRestart") == retvOk) {
+        if(RS422.Reply.NameIs("Ok")) {
+            uint32_t Sz=0;
+            uint16_t crc=0;
+            Printf("FW start transmission\r");
+            while(FwUpdater.ReadFileOnDiskToBuff(Sz, crc) == retvOk) {
+                Iwdg::Reload();
+//                Printf("ReadFile: Sz=%u crc=0x%X\r", Sz, crc);
+                // Send data away
+                if (RS422.SendCmdAndTransmitBuf(TIMEOUT_LONG_ms, FwUpdater.Buf, Sz, "UpdateFwWrite", "%u 0x%X", Sz, crc) == retvOk) {
+                    if(RS422.Reply.NameIs("Ok")) {;}
+                    else {
+                        Printf("Send data failure. Reply '%S'\r", RS422.Reply.Name);
+                        return;
+                    }
+                }
+                else {
+                    Printf("Send data failure TimeOut (NoAnswer)\r");
+                    return;
+                }
+            } // while
+            // End file
+            Iwdg::Reload();
+            FwUpdater.CalcCrcOfFullyFile(crc);
+//            Printf("CrcTotal 0x%X\r", crc);
+            Iwdg::Reload();
+            if (RS422.SendCmd(TIMEOUT_LONG_ms, 1, "UpdateFwCheckAndRun", "0x%X", crc) == retvOk) {
+                if(RS422.Reply.NameIs("Ok")) {
+                    FwUpdater.DeliteFileOnDisk();
+                    Printf("Update FW completed\r");
+                }
+                else
+                    Printf("Check FW failure. Reply '%S'\r", RS422.Reply.Name);
+            }
+            Iwdg::Reload();
+        }
+        else
+            Printf("Initiate update failure. Reply '%S'\r", RS422.Reply.Name);
+    } // if
+}
+ *
+ *
+ *  === Upgrade from UART (RX) ===
+void OnCmd(Shell_t *PShell) {
+    Cmd_t *PCmd = &PShell->Cmd;
+    // Handle command
+    if(PCmd->NameIs("Ping")) PShell->Ok();
+
+#if 1 // ==== FW Update ====
+    else if(PCmd->NameIs("UpdateFwRestart")) {
+        FwUpdater.Restart();
+        PShell->Ok();
+    }
+    else if(PCmd->NameIs("UpdateFwWrite")) {
+        Iwdg::Reload();
+        int32_t Sz;
+        if(PCmd->GetNext<int32_t>(&Sz) != retvOk or Sz < 1 or Sz > FW_UPD_BUF_SZ) { PShell->BadParam(); return; }
+        uint32_t crc32;
+        if(PCmd->GetNext<uint32_t>(&crc32) != retvOk) { PShell->BadParam(); return; }
+//        Printf("WriteFile: Sz=%u crc=0x%X\r", Sz, crc32);
+        if(PShell->ReceiveBinaryToBuf(FwUpdater.Buf, Sz, TIMEOUT_LONG_ms) == retvOk) {
+            Iwdg::Reload();
+            uint16_t crc16 = crc32;
+            switch(FwUpdater.WriteBuf(Sz, &crc16)) {
+                case retvOk:       PShell->Ok(); break;
+                case retvTimeout:  PShell->Timeout(); break;
+                case retvCRCError: PShell->CRCError(); break;
+                default: PShell->Failure(); break;
+            }
+        }
+        else PShell->Failure();
+    }
+    else if(PCmd->NameIs("UpdateFwCheckAndRun")) {
+        Iwdg::Reload();
+        uint32_t crc32;
+        if(PCmd->GetNext<uint32_t>(&crc32) != retvOk) { PShell->BadParam(); return; }
+        uint16_t crc16 = crc32;
+        switch(FwUpdater.CheckItAll(&crc16)) {
+            case retvOk:
+                PShell->Ok();
+                chThdSleepMilliseconds(45);
+                FwUpdater.SwitchAndRun();
+                break;
+            case retvCRCError: PShell->CRCError(); break;
+            default: PShell->Failure(); break;
+        }
+    }
+#endif
+}
+ *
+ */
