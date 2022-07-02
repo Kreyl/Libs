1f4792cf0c990af2cb6736deeca302ac4b67eca2
kreyl <laystoll@yandex.ru>
2022-07-02 17:31:53 +0300

    + F072 ADC

---
+++ kl_lib/lib/adcF072.cpp
@@ -0,0 +1,171 @@
+/*
+ * adc_f2.cpp
+ *
+ *  Created on: 09.04.2022
+ *      Author: kreyl
+ */
+
+#include <adcF072.h>
+#include "board.h"
+#include "MsgQ.h"
+#include "shell.h"
+
+#if ADC_REQUIRED
+
+Adc_t Adc;
+#ifdef ADC_TIM
+static const Timer_t ITmr(ADC_TIM);
+#endif
+
+// Wrapper for IRQ
+extern "C"
+void AdcRdyIrq(void *p, uint32_t flags) {
+    chSysLockFromISR();
+    Adc.IOnDmaIrq();
+    chSysUnlockFromISR();
+}
+
+void Adc_t::IOnDmaIrq() {
+    dmaStreamDisable(PDma);
+    // Switch buffers
+    if(PBufW == &IBuf1) { PBufW = &IBuf2; PBufR = &IBuf1; }
+    else                { PBufW = &IBuf1; PBufR = &IBuf2; }
+    // Restart DMA in case of Periodic Conversion Mode
+    if(Periodic) {
+        dmaStreamSetMemory0(PDma, PBufW->data());
+        dmaStreamSetTransactionSize(PDma, PBufW->size());
+        dmaStreamSetMode(PDma, ADC_DMA_MODE);
+        dmaStreamEnable(PDma);
+    }
+    // Signal event
+    if(ICallback != nullptr) ICallback();
+}
+
+void Adc_t::Init() {
+    rccResetADC1();
+    rccEnableADC1(FALSE); // Enable digital clock
+    // Configure clock
+//    ADC1->CFGR2 = (0b01 << 30); // Clock: PCLK/2
+    ADC1->CFGR2 = (0b00 << 30); // Clock: ADCCLK, will be turned in by ADC itself
+    // ==== Setup channels ====
+    EnableVref();
+    uint32_t ChnlCnt = AdcSetup.Channels.size();
+    uint32_t BufSz = ChnlCnt * (uint32_t)AdcSetup.Oversampling;
+    IBuf1.resize(BufSz);
+    IBuf2.resize(BufSz);
+    ADC1->CHSELR = 0; // Reset it
+    for(uint32_t i=0; i<ChnlCnt; i++) {
+        const AdcChannel_t& Chnl = AdcSetup.Channels[i];
+        if(Chnl.GPIO != nullptr) PinSetupAnalog(Chnl.GPIO, Chnl.Pin);
+        ADC1->CHSELR |= (1 << Chnl.ChannelN);
+    }
+    ADC1->SMPR = (uint32_t)AdcSetup.SampleTime; // Setup sampling time
+    ICallback = AdcSetup.DoneCallback;
+    // Calibrate
+    uint32_t cnt=0;
+    ADC1->CR |= ADC_CR_ADCAL;   // Start calibration
+    while(BitIsSet(ADC1->CR, ADC_CR_ADCAL)) {
+        if(cnt++ >= 63000) {
+            Printf("ADC calib fail\r");
+            return;
+        }
+    }
+    // Enable ADC
+    ADC1->CR |= ADC_CR_ADEN;   // Enable ADC
+    while(!BitIsSet(ADC1->ISR, ADC_ISR_ADRDY)); // Wait until ADC is ready
+    // ==== DMA ====
+    PDma = dmaStreamAlloc(ADC_DMA, IRQ_PRIO_MEDIUM, AdcRdyIrq, nullptr);
+    dmaStreamSetPeripheral(PDma, &ADC1->DR);
+    dmaStreamSetMode      (PDma, ADC_DMA_MODE);
+    // en DMA request, DMA is in circ mode
+    ADC1->CFGR1 = ADC_CFGR1_DMACFG | ADC_CFGR1_DMAEN;
+}
+
+static bool IsEnabled() { return (ADC1->CR & ADC_CR_ADEN); }
+
+// Not used directly, only by StartMeasurement etc.
+static void Calibrate() {
+    ADC1->CR |= ADC_CR_ADCAL;        // Start calibration
+    while((ADC1->CR & ADC_CR_ADCAL) != 0);   // Let it to complete
+    // ADEN bit cannot be set during ADCAL=1 and 4 ADC clock cycle after the ADCAL bit is cleared by hardware(end of the calibration).
+    for(volatile uint32_t i=0; i<99; i++) __NOP();
+}
+
+void Adc_t:: Stop() {
+    dmaStreamDisable(PDma);
+    if(IsEnabled()) {
+        SET_BIT(ADC1->CR, ADC_CR_ADSTP);    // Stop any ongoing conversion
+        while(READ_BIT(ADC1->CR, ADC_CR_ADSTP) != 0);   // Let it to complete
+        ADC1->CR |= ADC_CR_ADDIS; // Disable
+        while(IsEnabled());   // Let it to complete
+    }
+}
+
+void Adc_t::Deinit() {
+    Stop();
+#ifdef ADC_TIM
+    ITmr.Deinit();
+#endif
+    DisableVref();
+    rccDisableADC1(); // Disable clock
+}
+
+void Adc_t::EnableVref()  { ADC1_COMMON->CCR |= ADC_CCR_VREFEN; }
+void Adc_t::DisableVref() { ADC1_COMMON->CCR &= ADC_CCR_VREFEN; }
+
+// Service routine
+void Adc_t::DisableCalibrateEnableSetDMA() {
+    Stop();
+    Calibrate();
+    // Enable
+    SET_BIT(ADC1->ISR, ADC_ISR_ADRDY);  // Clear ADRDY bit by writing 1 to it
+    SET_BIT(ADC1->CR, ADC_CR_ADEN);     // Enable ADC
+    while(READ_BIT(ADC1->ISR, ADC_ISR_ADRDY) == 0);   // Let it to complete
+    // Disable continuous mode
+    ADC1->CFGR1 &= ~ADC_CFGR1_CONT;
+    // Disable trigger
+    ADC1->CFGR1 &= ~ADC_CFGR1_EXTEN;
+    // DMA
+    dmaStreamSetMemory0(PDma, PBufW->data());
+    dmaStreamSetTransactionSize(PDma, PBufW->size());
+    dmaStreamSetMode(PDma, ADC_DMA_MODE);
+    dmaStreamEnable(PDma);
+}
+
+// Start sequence conversion and run callback when done
+void Adc_t::StartSingleMeasurement() {
+    DisableCalibrateEnableSetDMA();
+    Periodic = false;
+    StartConversion();
+}
+
+// Start periodic conversions, run callback every time when done
+#ifdef ADC_TIM
+void Adc_t::StartPeriodicMeasurement(uint32_t FSmpHz) {
+    DisableCalibrateEnableSetDMA();
+    // Enable trigger
+    ADC1->CFGR1 &= ~(0b111UL << ADC_CFGR1_EXTSEL_Pos); // Clear it
+    ADC1->CFGR1 |=  (0b000UL << ADC_CFGR1_EXTSEL_Pos); // 000 = TRG0 = TIM6_TRGO
+    ADC1->CFGR1 |=  (0b01UL  << ADC_CFGR1_EXTEN_Pos);  // trigger detection on the rising edge
+    Periodic = true;
+    StartConversion();
+    // Setup timer
+    ITmr.Init();
+    ITmr.SetUpdateFrequencyChangingBoth(FSmpHz * (uint32_t)AdcSetup.Oversampling);
+    ITmr.SelectMasterMode(mmUpdate);
+    ITmr.Enable();
+}
+#endif
+
+void Adc_t::StartContinuosMeasurement() {
+    DisableCalibrateEnableSetDMA();
+    Periodic = true;
+    // Enable continuous mode
+    ADC1->CFGR1 |= ADC_CFGR1_CONT;
+    StartConversion();
+}
+
+uint32_t Adc_t::Adc2mV(uint32_t AdcChValue, uint32_t VrefValue) {
+    return ((ADC_VREFINT_CAL_mV * (uint32_t)ADC_VREFINT_CAL / ADC_MAX_VALUE) * AdcChValue) / VrefValue;
+}
+#endif  // ADC_REQUIRED
---
+++ kl_lib/lib/adcF072.h
@@ -0,0 +1,84 @@
+/*
+ * kl_adc.h
+ *
+ *  Created on: 12.04.2013
+ *      Author: kreyl
+ */
+
+#pragma once
+
+#include "kl_lib.h"
+#include "board.h"
+#include <vector>
+
+#if ADC_REQUIRED
+
+#define ADC_MAX_VALUE           4095    // const: 2^12
+
+// Do not change, see ref man p.211
+#define ADC_TEMPERATURE_CHNL    16
+#define ADC_VREFINT_CHNL        17
+#define ADC_VBAT_CHNL           18
+#define ADC_MAX_SEQ_LEN         16
+// See datasheet, search VREFINT_CAL
+#define ADC_VREFINT_CAL_mV      3300UL
+#define ADC_VREFINT_CAL     (*(volatile uint16_t*)0x1FFFF7BA)
+
+// ADC sampling_times
+enum AdcSampleTime_t {
+    ast1d5Cycles    = 0,
+    ast7d5Cycles    = 1,
+    ast13d5Cycles   = 2,
+    ast28d5Cycles   = 3,
+    ast41d5Cycles   = 4,
+    ast55d5Cycles   = 5,
+    ast71d5Cycles   = 6,
+    ast239d5Cycles  = 7
+};
+
+struct AdcChannel_t {
+    GPIO_TypeDef *GPIO;
+    uint32_t Pin;
+    uint32_t ChannelN;
+};
+
+struct AdcSetup_t {
+    uint32_t SampleTime;
+    enum Oversampling_t : uint32_t {
+            oversmpDis=1, oversmp2=2, oversmp4=4, oversmp8=8, oversmp16=16,
+            oversmp32=32, oversmp64=64, oversmp128=128, oversmp256=256
+        } Oversampling;
+    ftVoidVoid DoneCallback;
+    std::vector<AdcChannel_t> Channels;
+};
+
+typedef std::vector<uint16_t> AdcBuf_t;
+
+class Adc_t {
+private:
+    const stm32_dma_stream_t *PDma;
+    AdcBuf_t IBuf1, IBuf2, *PBufW = &IBuf1, *PBufR = &IBuf2;
+    ftVoidVoid ICallback = nullptr;
+    void StartConversion() { ADC1->CR |= ADC_CR_ADSTART; }
+    void DisableCalibrateEnableSetDMA(); // Service routine
+public:
+    bool Periodic;
+    AdcBuf_t& GetBuf() { return *PBufR; }
+    void Init();
+    void Deinit();
+    void EnableVref();
+    void DisableVref();
+    void StartSingleMeasurement();
+    void StartPeriodicMeasurement(uint32_t FSmpHz);
+    void StartContinuosMeasurement();
+    void Stop();
+    uint32_t Adc2mV(uint32_t AdcChValue, uint32_t VrefValue);
+//    uint32_t GetResult(uint8_t AChannel);
+    // Inner use
+    void IOnDmaIrq();
+};
+
+extern Adc_t Adc;
+extern const AdcSetup_t AdcSetup;
+
+#endif // ADC_REQUIRED
